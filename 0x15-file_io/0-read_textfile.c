42e8bad0a458ffb670402ac65f132cb22f9a7e87
-- <karima.jarroug.1996@gmail.com>
2023-05-08 18:18:18 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	oo = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
+	r = read(oo, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (oo == -1 || r == -1 || w == -1 || w != r)
